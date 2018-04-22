022cf755bac5e61bb05f96dec076006063cb41cd
SherMM <helioblast86@gmail.com>
2018-04-22 18:33:41 -0400

    added dna-to-rna transcription problem

--- rna/src/rna/core.clj
+++ rna/src/rna/core.clj
@@ -1,7 +1,22 @@
 (ns rna.core
   (:gen-class))
 
+
+(defn transcribe
+	[nucleotide]
+	(if (= nucleotide "T")
+		"U"
+		nucleotide))
+
+(defn transcribe-to-rna
+	([dna]
+		(transcribe-to-rna dna ""))
+	([dna rna]
+		(if (empty? dna)
+			rna
+			(recur (subs dna 1) (str rna (transcribe (subs dna 0 1)))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (transcribe-to-rna (clojure.string/trim (slurp (nth args 0))))))
