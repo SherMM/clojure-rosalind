4a95c0c810ecc2559ac632c0cfa69d02487de4e5
SherMM <helioblast86@gmail.com>
2018-06-25 21:56:55 -0400

    finished hamming distance problem

--- hamm/src/hamm/core.clj
+++ hamm/src/hamm/core.clj
@@ -1,7 +1,18 @@
 (ns hamm.core
-  (:gen-class))
+  (:require [clojure.string :as str]))
+
+
+(defn count-mismatches
+	[[x y]]
+	(if (= x y) 0 1))
+
+
+(defn calculate-hamming
+  [strands]
+  (reduce + (map count-mismatches 
+  	(apply map list strands))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (calculate-hamming (str/split (slurp (nth args 0)) #"\n"))))
